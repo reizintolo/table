d9e51b6e38a41c273eacdfae68ce0fc61199a9d3
Daniel Malvar <danielmalvar@MacBook-Pro-de-Daniel.local>
2015-06-30 01:24:45 +0200

    add delete

--- app/dmvs/dmvs.js
+++ app/dmvs/dmvs.js
@@ -9,6 +9,46 @@ myApp.controller('DmvsController', ['$scope','$http', function($scope,$http) {
     $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json')
             .success(function(data) {
               $scope.myData = data;
-            });
+            });                  
+            
+        $scope.gridOptions = {};
+        $scope.Delete = function(row) {
+            var index = $scope.gridOptions.data.indexOf(row.entity);
+            $scope.gridOptions.data.splice(index, 1);
+        };
+        $scope.Add = function(row) {
+            var index = $scope.gridOptions.data.indexOf(row.entity);
+            $scope.gridOptions.data.splice(index+1, 0,"");
+        };
+        
+        $scope.gridOptions.columnDefs = [{
+            name: 'firstName',
+            field: 'firstName'
+        }, {
+            name: 'lastNamer',
+            field: 'firstName'
+        }, {
+            name: 'ShowScope',
+            cellTemplate: '<button class="btn primary" ng-click="grid.appScope.Delete(row)">Delete Me</button>'+
+            '<button class="btn primary" ng-click="grid.appScope.Add(row)">Add Me</button>'
+        }];
+        
+        $scope.gridOptions.data = [{
+            "firstName": "Cox",
+            "lastName": "Carney",
+            "company": "Enormo",
+            "employed": true
+        }, {
+            "firstName": "Lorraine",
+            "lastName": "Wise",
+            "company": "Comveyer",
+            "employed": false
+        }, {
+            "firstName": "Nancy",
+            "lastName": "Waters",
+            "company": "Fuelton",
+            "employed": false
+        }];
+
 
 }]);
